726ef0ed480e6c8a109ba6a6fbc773aade2cfe6f
fabiobaptista <fhbaptista@gmail.com>
2015-12-12 21:51:20 -0200

    projeto final

--- Knockout-final/Knockout-final/Controllers/HomeController.cs
+++ Knockout-final/Knockout-final/Controllers/HomeController.cs
@@ -6,11 +6,31 @@ using System.Web.Mvc;
 
 namespace Knockout_final.Controllers
 {
+    public class Model {
+        public static List<dynamic> lista = new List<dynamic>() { 
+                new { titulo= "Titulo item 1", conteudo= "Conteúdo obtido de GetLIst()" },
+		        new { titulo= "Titulo item 2", conteudo= "Conteúdo obtido de GetLIst()" },
+		        new { titulo= "Titulo item 3", conteudo= "Conteúdo obtido de GetLIst()" }
+            };
+    }
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
         }
+
+        public JsonResult GetList() {
+
+            return Json(Model.lista);
+        }
+
+        public ActionResult Create(string titulo, string conteudo) {
+
+            Model.lista.Add(new { titulo = titulo, conteudo = conteudo });
+
+            return new HttpStatusCodeResult(200);
+        }
+
     }
 }
